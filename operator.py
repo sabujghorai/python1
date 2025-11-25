3a9460879a64c3f1c19879a2ba68243954f08d68
sabujghorai <ghoraisabuj4@gmail.com>
2025-11-26 02:16:50 +0530

    Create operator.py
    operator

---
+++ operator.py
@@ -0,0 +1,32 @@
+#Arithmatic operartor(+,-,*,/,%,**)
+a = 5
+b = 2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) # prints the remainder
+print(a**b) # a^b
+
+# Relational Operator(prints True or False only)
+a = 5
+b = 2
+print(a==b)
+print(a!=b)
+
+ # Assignment operator(=,+=,-=,/=,%=,**=)
+a = 5
+a += 10 # prints a+10 = 15
+a **= 5 # power operator 15^5 = 759375
+print(a)
+
+
+# Logical operator (not,and ,or)
+print(not False) # oposites the bollean value
+print(not True)
+value1 = True
+value2 = True
+value3 = False
+print("And operator :",value1 and value2)# prints true if two values are true
+print("or operator :",value2 or value3) # prints true(any one is true then prints true)
+
