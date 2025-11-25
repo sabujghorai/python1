91e578a85b70444be9aeee74d826c2ccc0f44423
sabujghorai <ghoraisabuj4@gmail.com>
2025-11-26 01:53:30 +0530

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,10 @@
+name = "sabuj"
+age = 20
+num = 25.43
+abc = True
+a = None
+print(type(name)) # string datatype
+print(type(age)) # integer datatype
+print(type(num)) # float datatype
+print(type(abc)) # boolean datatype
+print(type(a))
