ac0752e660ef75a915bc1504e4188f559bc42028
sabujghorai <ghoraisabuj4@gmail.com>
2025-11-26 12:22:57 +0530

    Create userinput.py

---
+++ userinput.py
@@ -0,0 +1,4 @@
+#This is the code to find the average if two numbers
+a = int(input("enter a number"))
+b = int(input("enter another number:")
+        print("The average of the two number is :",(a+b)/2)
